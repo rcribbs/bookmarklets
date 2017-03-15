145c0f89092c8141a2b486658e7159dc43d99b4f
Rohan <rohan.cribbs@gmail.com>
2017-03-14 22:28:49 -0700

    Make compile override stub and use rawgit instead of raw.github.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO: Use git remote to dynamically set url for rawgit instead of hardcoded.
 for filename in *.js; do
   name=${filename%.js}
   echo "Compiling '$filename'..."
@@ -7,5 +8,5 @@ for filename in *.js; do
   echo "Creating bookmarklet..."
   echo "javascript:" > "marklets/$name.min.mark.js"; cat "min/$name.min.js" >> "marklets/$name.min.mark.js"
   echo "Creating stub-version bookmarklet..."
-  echo -n "javascript:(function(){_my_script=document.createElement('SCRIPT');_my_script.type='text/javascript';_my_script.src='https://raw.githubusercontent.com/rcribbs/bookmarklets/master/min/$name.min.js';document.getElementsByTagName('head')[0].appendChild(_my_script);})();" >> "stubs/$name.url"
+  echo -n "javascript:(function(){_my_script=document.createElement('SCRIPT');_my_script.type='text/javascript';_my_script.src='https://cdn.rawgit.com/rcribbs/bookmarklets/master/min/$name.min.js';document.getElementsByTagName('head')[0].appendChild(_my_script);})();" > "stubs/$name.url"
 done
