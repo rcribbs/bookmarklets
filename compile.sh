f66e17adf4d0e663fde34d18e044203ab02d3943
Rohan <rohan.cribbs@gmail.com>
2017-03-15 00:02:40 -0700

    Use callback and noconflict to be sure to use self-loaded jQuery.

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ for filename in src/*.js; do
   echo "Compiling '$filename'..."
   babel $filename | uglifyjs -cmo "min/$name.min.js"
   echo "Creating bookmarklet..."
-  echo "javascript:" > "marklets/$name.min.mark.js"; cat "min/$name.min.js" >> "marklets/$name.min.mark.js"
+  echo -n "javascript:" > "marklets/$name.min.mark.js"; cat "min/$name.min.js" >> "marklets/$name.min.mark.js"
   echo "Creating stub-version bookmarklet..."
   echo -n "javascript:(function(){_$name=document.createElement('SCRIPT');_$name.type='text/javascript';_$name.src='https://cdn.rawgit.com/rcribbs/bookmarklets/master/min/$name.min.js?ranid='+(Math.random());document.getElementsByTagName('head')[0].appendChild(_$name);})();" > "stubs/$name.url"
 done
--- src/feedly_mobile.js
+++ src/feedly_mobile.js
@@ -1,77 +1,78 @@
-(function() {
-
-function addcss(css){
-	var head = document.getElementsByTagName('head')[0];
-	var s = document.createElement('style');
-	s.setAttribute('type', 'text/css');
-	if (s.styleSheet) {   // IE
-		s.styleSheet.cssText = css;
-	} else {              // the world
-		s.appendChild(document.createTextNode(css));
+function addcss(css) {
+  var head = document.getElementsByTagName('head')[0];
+  var s = document.createElement('style');
+  s.setAttribute('type', 'text/css');
+  if (s.styleSheet) {   // IE
+    s.styleSheet.cssText = css;
+  } else {              // the world
+    s.appendChild(document.createTextNode(css));
   }
   head.appendChild(s);
 }
 
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.1.1.slim.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-script.onreadystatechange = handler;
-script.onload = handler;
-
-head.appendChild(script);
-
 function handler() {
-  var cssStyle = `
-  #feedlyFrame {
-    margin-left: 0px !important;
-  }
+  var $j = jQuery.noConflict();
+  (function($) {
+    var cssStyle = `
+    #feedlyFrame {
+      margin-left: 0px !important;
+    }
 
-  #feedlyTabsHolder {
-    display: none;
-  }
+    #feedlyTabsHolder {
+      display: none;
+    }
 
-  .pro {
-    display: none;
-  }
+    .pro {
+      display: none;
+    }
 
-  div.hercule-search input {
-    font-size: .7rem !important;
-  }
+    div.hercule-search input {
+      font-size: .7rem !important;
+    }
 
-  search-bar-right-col {
-    float: none;
-  }
+    search-bar-right-col {
+      float: none;
+    }
 
-  div.profile-bubble, i.feedly-logo {
-    display: none !important;
-  }
+    div.profile-bubble, i.feedly-logo {
+      display: none !important;
+    }
 
-  #searchBarFX {
-    width: 100% !important;
-  }
+    #searchBarFX {
+      width: 100% !important;
+    }
 
-  #headerBarFX {
-    left: 0px !important;
-    top: auto !important;
-  }
-  `;
-
-
+    #headerBarFX {
+      left: 0px !important;
+      top: auto !important;
+    }
+    `;
 
 
-  $('#feedlyTabsUnpin').click();
 
-  var pageHeader = $('#feedlyPageFX h1')
-  pageHeader.removeClass('col-xs-8');
-  pageHeader.addClass('col-xs-12');
 
-  var pageHeaderOptions = $('#feedlyPageFX div.extra')
-  pageHeaderOptions.removeClass('col-xs-4');
-  pageHeaderOptions.addClass('col-xs-12');
+    $('#feedlyTabsUnpin').click();
 
-  addcss(cssStyle);
+    var pageHeader = $('#feedlyPageFX h1');
+    pageHeader.removeClass('col-xs-8');
+    pageHeader.addClass('col-xs-12');
+
+    var pageHeaderOptions = $('#feedlyPageFX div.extra');
+    pageHeaderOptions.removeClass('col-xs-4');
+    pageHeaderOptions.addClass('col-xs-12');
+
+    addcss(cssStyle);
+  })($j);
 }
 
+(function() {
+  var head = document.getElementsByTagName('head')[0];
+  var script = document.createElement('script');
+  script.src = 'https://code.jquery.com/jquery-3.1.1.slim.min.js';
+  script.type = 'text/javascript';
+
+  script.onreadystatechange = handler;
+  script.onload = handler;
+
+  head.appendChild(script);
 })();
