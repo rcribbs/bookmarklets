1a6db5bb49d73b69702ffb00b17d6b2be166a18b
Rohan <rohan.cribbs@gmail.com>
2017-03-14 21:10:41 -0700

    Update script to create bookmarklets for easy copying. Created marklets folder.

--- compile.sh
+++ compile.sh
@@ -2,5 +2,8 @@
 
 for filename in *.js; do
   name=${filename%.js}
+  echo "Compiling '$filename'..."
   babel $filename | uglifyjs -cmo "min/$name.min.js"
+  echo "Creating bookmarklet..."
+  echo "javascript:" > "marklets/$name.min.mark.js"; cat "min/$name.min.js" >> "marklets/$name.min.mark.js"
 done
