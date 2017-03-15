1c4e77a2dc1f05cf9490dbd755e2a842f9451ae1
Rohan <rohan.cribbs@gmail.com>
2017-03-14 23:10:06 -0700

    Run script only after adding jquery.

--- src/feedly_mobile.js
+++ src/feedly_mobile.js
@@ -12,58 +12,66 @@ function addcss(css){
   head.appendChild(s);
 }
 
-var cssStyle = `
-#feedlyFrame {
-  margin-left: 0px !important;
-}
-
-#feedlyTabsHolder {
-  display: none;
-}
-
-.pro {
-  display: none;
-}
-
-div.hercule-search input {
-  font-size: .7rem !important;
-}
-
-search-bar-right-col {
-  float: none;
-}
-
-div.profile-bubble, i.feedly-logo {
-  display: none !important;
-}
-
-#searchBarFX {
-  width: 100% !important;
-}
-
-#headerBarFX {
-  left: 0px !important;
-  top: auto !important;
-}
-`;
-
-
-
 var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.1.1.slim.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-$('#feedlyTabsUnpin').click();
+script.onreadystatechange = handler;
+script.onload = handler;
 
-var pageHeader = $('#feedlyPageFX h1')
-pageHeader.removeClass('col-xs-8');
-pageHeader.addClass('col-xs-12');
+head.appendChild(script);
 
-var pageHeaderOptions = $('#feedlyPageFX div.extra')
-pageHeaderOptions.removeClass('col-xs-4');
-pageHeaderOptions.addClass('col-xs-12');
+function handler() {
+  var cssStyle = `
+  #feedlyFrame {
+    margin-left: 0px !important;
+  }
 
-addcss(cssStyle);
+  #feedlyTabsHolder {
+    display: none;
+  }
+
+  .pro {
+    display: none;
+  }
+
+  div.hercule-search input {
+    font-size: .7rem !important;
+  }
+
+  search-bar-right-col {
+    float: none;
+  }
+
+  div.profile-bubble, i.feedly-logo {
+    display: none !important;
+  }
+
+  #searchBarFX {
+    width: 100% !important;
+  }
+
+  #headerBarFX {
+    left: 0px !important;
+    top: auto !important;
+  }
+  `;
+
+
+
+
+  $('#feedlyTabsUnpin').click();
+
+  var pageHeader = $('#feedlyPageFX h1')
+  pageHeader.removeClass('col-xs-8');
+  pageHeader.addClass('col-xs-12');
+
+  var pageHeaderOptions = $('#feedlyPageFX div.extra')
+  pageHeaderOptions.removeClass('col-xs-4');
+  pageHeaderOptions.addClass('col-xs-12');
+
+  addcss(cssStyle);
+}
 
 })();
