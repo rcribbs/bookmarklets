67f9945edb4224212bef572b46a9e45a74ac0b24
Rohan <rohan.cribbs@gmail.com>
2017-03-15 13:14:22 -0700

    Use tagging system to update cdn for rawgit.

--- compile.sh
+++ compile.sh
@@ -2,6 +2,9 @@
 
 # TODO: Use git remote to dynamically set url for rawgit instead of hardcoded.
 for filename in src/*.js; do
+  latest_tag=$(git describe --abbrev=0 --tags)
+  tag=${1:-$latest_tag}
+  echo $tag
   relative_filename=${filename#src/}
   name=${relative_filename%.js}
   echo "Compiling '$filename'..."
@@ -9,5 +12,5 @@ for filename in src/*.js; do
   echo "Creating bookmarklet..."
   echo -n "javascript:" > "marklets/$name.min.mark.js"; cat "min/$name.min.js" >> "marklets/$name.min.mark.js"
   echo "Creating stub-version bookmarklet..."
-  echo -n "javascript:(function(){_$name=document.createElement('SCRIPT');_$name.type='text/javascript';_$name.src='https://cdn.rawgit.com/rcribbs/bookmarklets/master/min/$name.min.js?ranid='+(Math.random());document.getElementsByTagName('head')[0].appendChild(_$name);})();" > "stubs/$name.url"
+  echo -n "javascript:(function(){_$name=document.createElement('SCRIPT');_$name.type='text/javascript';_$name.src='https://cdn.rawgit.com/rcribbs/bookmarklets/$tag/min/$name.min.js?ranid='+(Math.random());document.getElementsByTagName('head')[0].appendChild(_$name);})();" > "stubs/$name.url"
 done
