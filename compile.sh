a422e9384cbc1918403873c58d9199f238e67a64
Rohan <rohan.cribbs@gmail.com>
2017-03-14 22:20:47 -0700

    Modify compile to create no spaces tiny bookmarklets for shitty browsers.
    I'm looking at you SAMSUNG 'internet'!!! （╬ಠ益ಠ)

--- compile.sh
+++ compile.sh
@@ -6,4 +6,6 @@ for filename in *.js; do
   babel $filename | uglifyjs -cmo "min/$name.min.js"
   echo "Creating bookmarklet..."
   echo "javascript:" > "marklets/$name.min.mark.js"; cat "min/$name.min.js" >> "marklets/$name.min.mark.js"
+  echo "Creating stub-version bookmarklet..."
+  echo -n "javascript:(function(){_my_script=document.createElement('SCRIPT');_my_script.type='text/javascript';_my_script.src='https://raw.githubusercontent.com/rcribbs/bookmarklets/master/min/$name.min.js';document.getElementsByTagName('head')[0].appendChild(_my_script);})();" >> "stubs/$name.url"
 done
